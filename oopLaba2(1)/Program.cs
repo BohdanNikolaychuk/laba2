c6b5f462a3b340488e35a2817f8ed7008e71de00
BohdanNikolaychuk <nikolaychukbm@gmail.com>
2020-04-25 22:34:55 +0300

    1
    1

---
+++ oopLaba2(1)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace oopLaba2_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть номер прізвища в списку");
+           
+            int i = int.Parse(Console.ReadLine());
+            switch (i)
+            {
+                case 1:
+                Console.WriteLine("A");
+                    break;
+                case 2:
+                    Console.WriteLine("Б");
+                    break;
+                case 3:
+                    Console.WriteLine("В");
+                    break;
+                case 4:
+                    Console.WriteLine("Г");
+                    break;
+                case 5:
+                    Console.WriteLine("Д");
+                    break;
+                default:
+                    Console.WriteLine("не введений номер");
+                    break;
+            }
+
+        }
+    }
+}
